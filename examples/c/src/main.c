6dcd68cf0cd5de3a0e5e8d11b2668137e174fc2b
Rob Loach <robloach@gmail.com>
2022-06-07 00:27:31 -0400

    redesign

---
+++ examples/c/src/main.c
@@ -0,0 +1,30 @@
+#include "raylib-wasm.h"
+
+void Init() {
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    InitWindow(800, 450, "raylib-wasm [core] example - basic window");
+    SetTargetFPS(60);
+}
+
+void UpdateDrawFrame() {
+    // Update
+    //----------------------------------------------------------------------------------
+    // TODO: Update your variables here
+    //----------------------------------------------------------------------------------
+
+    // Draw
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Congrats! You created your first raylib-cpp window!", 190, 200, 20, LIGHTGRAY);
+
+    EndDrawing();
+    //----------------------------------------------------------------------------------
+
+    // if (WindowShouldClose()) {
+    //     CloseWindow();
+    // }
+}
